7990a0e1ff2aac29c87404f555a03d567e81920a
Henry Qi <henryqi222@gmail.com>
2017-02-07 14:12:14 -0800

    6 fix

--- codewars/6-camelcasemethod.js
+++ codewars/6-camelcasemethod.js
@@ -1,7 +1,3 @@
-// String.prototype.camelCase=function(){
-//   return Object.values(this).join("").split(" ").map(elem => elem.charAt(0).toUpperCase() + elem.slice(1)).join("")
-// }
-
 String.prototype.camelCase=function(){
-	return this.split("").join("").split(" ").map(elem => elem.charAt(0).toUpperCase() + elem.slice(1)).join("")
+	return this.split(" ").map(elem => elem.charAt(0).toUpperCase() + elem.slice(1)).join("")
 }
