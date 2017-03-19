fabbb720c98b6ddaa3031956241f87348818e66b
Henry Qi <henryqi222@gmail.com>
2017-03-19 16:54:43 -0700

    redo merge

--- 4semcs6hr/mergesort.js
+++ 4semcs6hr/mergesort.js
@@ -1,37 +1,22 @@
 const mergeSort = nums => {
-    if (nums.length < 2) {
-        return nums
-    }
-    const middle = Math.floor(nums.length/2);
-    const left = nums.slice(0, middle);
-    const right = nums.slice(middle);
-
-    const sortedLeft = mergeSort(left);
-    const sortedRight = mergeSort(right);
-
-    return stitch(sortedLeft, sortedRight);
-};
+	if (nums.length < 2) {
+		return nums;
+	}
+	let middle = Math.floor(nums.length/2);
+	let left = nums.slice(0, middle);
+	let right = nums.slice(middle);
+	
+	let sortedLeft = mergeSort(left);
+	let sortedRight = mergeSort(right);
+	
+	return stitch(sortedLeft, sortedRight);
+	
+}
 
 const stitch = (left, right) => {
-    const results = [];
-
-    while (left.length && right.length){
-        if (left[0] <= right[0]){
-            results.push(left.shift());
-        } else {
-            results.push(right.shift());
-        }
-    }
-
-    // while (left.length) {
-    //     results.push(left.shift());
-    // }
-    // while (right.length) {
-    //     results.push(right.shift());
-    // }
-    // return results;
-
-    // return results.concat(left, right);
-
-    return [...results, ...left, ...right];
+	const result = [];
+	while (left.length && right.length) {
+		result.push(left[0] < right[0] ? left.shift() : right.shift());
+	}
+	return [...result, ...left, ...right]
 }
